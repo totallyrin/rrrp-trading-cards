db793f17348f3d8b2e130865a338166fd74048ba
Lucy Woloszczuk <lucy.woloszczuk@gmail.com>
2024-05-17 19:06:15 -0400

    adds quick-home

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
-import {Avatar, Heading, HStack, IconButton, Spacer} from "@chakra-ui/react";
+import {Avatar, Heading, HStack, IconButton, LinkBox, LinkOverlay, Spacer} from "@chakra-ui/react";
 import {HamburgerIcon} from "@chakra-ui/icons";
+import NextLink from "next/link";
 
 export default function Navbar() {
 
@@ -7,9 +8,13 @@ export default function Navbar() {
     <HStack width="100%" p={5}>
       <IconButton aria-label="Menu" icon={<HamburgerIcon/>}/>
       <Spacer/>
-      <Heading size="md">RRRP Top Trumps</Heading>
+      <LinkBox>
+        <LinkOverlay as={NextLink} href="/">
+          <Heading size="lg">RRRP Top Trumps</Heading>
+        </LinkOverlay>
+      </LinkBox>
       <Spacer/>
-      <Avatar name="" src=""/>
+      <Avatar size="sm" name="" src=""/>
     </HStack>
   );
 }
