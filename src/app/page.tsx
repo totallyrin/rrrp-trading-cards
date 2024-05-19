823576ca51aa88b4fb0c3afb7e3f40c050eb3745
Lucy Woloszczuk <lucy.woloszczuk@gmail.com>
2024-05-19 14:11:44 -0400

    add padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import {Spacer, VStack} from "@chakra-ui/react";
+import { Spacer, VStack } from "@chakra-ui/react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <VStack height="100vh">
-      <Navbar/>
+    <VStack height="100vh" p={5}>
+      <Navbar />
       text
-      <Spacer/>
-      <Footer/>
+      <Spacer />
+      <Footer />
     </VStack>
   );
 }
