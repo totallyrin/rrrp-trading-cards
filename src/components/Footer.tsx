3d1c206034fb0e99afa52b0f7f016d0c41141b01
Lucy Woloszczuk <lucy.woloszczuk@gmail.com>
2024-05-19 14:12:02 -0400

    adds condensed prop + functionality

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,61 @@
-import {Button, HStack, LinkBox, LinkOverlay, Spacer, Text, useColorMode} from "@chakra-ui/react";
-import {MoonIcon, SunIcon} from "@chakra-ui/icons";
-import {GitHubIcon} from "@/components/icons/GitHubIcon";
+import {
+  Box,
+  Button,
+  Flex,
+  HStack,
+  IconButton,
+  LinkBox,
+  LinkOverlay,
+  Text,
+  useColorMode,
+} from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { GitHubIcon } from "@/components/icons/GitHubIcon";
 import NextLink from "next/link";
 
-export default function Footer() {
-  const {colorMode, toggleColorMode} = useColorMode();
+export default function Footer({ condensed = false }) {
+  const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <HStack p={5} width="100%">
-      <Button onClick={toggleColorMode} leftIcon={colorMode === "dark" ? <SunIcon/> :
-        <MoonIcon/>}>{colorMode === "light" ? "Dark" : "Light"} Mode</Button>
-      <Spacer/>
-      <Text as="samp" fontSize="xs">MADE WITH &#x1F49B; BY TOTALLYRIN</Text>
-      <Spacer/>
-      <LinkBox>
-        <LinkOverlay as={NextLink} href="https://github.com/totallyrin/rrrp-top-trumps" isExternal>
-          <Button rightIcon={<GitHubIcon/>}>GitHub</Button>
-        </LinkOverlay>
-      </LinkBox>
+    <HStack width="100%" justifyContent="space-between">
+      <Box flex="1 1 0">
+        {condensed ? (
+          <IconButton
+            onClick={toggleColorMode}
+            icon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
+            aria-label={`${colorMode === "light" ? "Dark" : "Light"} Mode`}
+          />
+        ) : (
+          <Button
+            onClick={toggleColorMode}
+            leftIcon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
+          >
+            {colorMode === "light" ? "Dark" : "Light"} Mode
+          </Button>
+        )}
+      </Box>
+      <Flex flex="1 1 0" justifyContent="center">
+        {condensed ? null : (
+          <Text as="samp" fontSize="xs" alignSelf="center">
+            MADE WITH &#x1F49B; BY TOTALLYRIN
+          </Text>
+        )}
+      </Flex>
+      <Flex flex="1 1 0" justifyContent="flex-end">
+        <LinkBox>
+          <LinkOverlay
+            as={NextLink}
+            href="https://github.com/totallyrin/rrrp-top-trumps"
+            isExternal
+          >
+            {condensed ? (
+              <IconButton icon={<GitHubIcon />} aria-label="GitHub" />
+            ) : (
+              <Button rightIcon={<GitHubIcon />}>GitHub</Button>
+            )}
+          </LinkOverlay>
+        </LinkBox>
+      </Flex>
     </HStack>
   );
 }
