9522b0f40ff280e4168f90c5411a91bf716c56c4
Lucy Woloszczuk <lucy.woloszczuk@gmail.com>
2024-05-19 14:15:40 -0400

    add popover and drawer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,111 @@
-import {Avatar, Heading, HStack, IconButton, LinkBox, LinkOverlay, Spacer} from "@chakra-ui/react";
-import {HamburgerIcon} from "@chakra-ui/icons";
+import {
+  Avatar,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  Heading,
+  HStack,
+  IconButton,
+  LinkBox,
+  LinkOverlay,
+  Popover,
+  PopoverAnchor,
+  PopoverBody,
+  PopoverContent,
+  PopoverFooter,
+  PopoverHeader,
+  PopoverTrigger,
+  useBoolean,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { ChevronRightIcon, CloseIcon, HamburgerIcon } from "@chakra-ui/icons";
 import NextLink from "next/link";
+import Footer from "@/components/Footer";
+import { signIn, useSession } from "next-auth/react";
 
 export default function Navbar() {
+  const { data: session } = useSession();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isPop, setPop] = useBoolean();
 
   return (
-    <HStack width="100%" p={5}>
-      <IconButton aria-label="Menu" icon={<HamburgerIcon/>}/>
-      <Spacer/>
-      <LinkBox>
-        <LinkOverlay as={NextLink} href="/">
-          <Heading size="lg">RRRP Top Trumps</Heading>
-        </LinkOverlay>
-      </LinkBox>
-      <Spacer/>
-      <Avatar size="sm" name="" src=""/>
-    </HStack>
+    <>
+      <Popover
+        isOpen={isPop}
+        onOpen={setPop.on}
+        onClose={setPop.off}
+        closeOnBlur={true}
+        closeOnEsc={true}
+        gutter={0}
+      >
+        <HStack width="100%" justifyContent="space-between" align="center">
+          <IconButton
+            aria-label="Menu"
+            icon={<HamburgerIcon />}
+            onClick={onOpen}
+          />
+          <LinkBox>
+            <LinkOverlay as={NextLink} href="/">
+              <Heading size="lg">RRRP Top Trumps</Heading>
+            </LinkOverlay>
+          </LinkBox>
+          {session ? (
+            <PopoverAnchor>
+              <PopoverTrigger>
+                <IconButton
+                  isRound
+                  aria-label="Profile"
+                  icon={<Avatar size="xs" name="" src="" />}
+                />
+              </PopoverTrigger>
+            </PopoverAnchor>
+          ) : (
+            <Button rightIcon={<ChevronRightIcon />} onClick={() => signIn()}>
+              Sign in
+            </Button>
+          )}
+        </HStack>
+        <Drawer isOpen={isOpen} onClose={onClose} placement="left">
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerHeader>
+              <Flex justifyContent="space-between">
+                RRRP Top Trumps
+                <IconButton
+                  aria-label="Close"
+                  onClick={onClose}
+                  size="sm"
+                  icon={<CloseIcon />}
+                />
+              </Flex>
+            </DrawerHeader>
+            <DrawerBody>text</DrawerBody>
+            <DrawerFooter>
+              <Footer condensed />
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
+        <PopoverContent m={5}>
+          <PopoverHeader>
+            <Flex justifyContent="space-between">
+              Welcome, {session?.user?.name}.
+              <IconButton
+                aria-label="Close"
+                onClick={setPop.off}
+                size="xs"
+                icon={<CloseIcon />}
+              />
+            </Flex>
+          </PopoverHeader>
+          <PopoverBody>text</PopoverBody>
+          <PopoverFooter></PopoverFooter>
+        </PopoverContent>
+      </Popover>
+    </>
   );
 }
