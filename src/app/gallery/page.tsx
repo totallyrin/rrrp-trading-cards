372d5ade2f4397d438feeec497838a3bab18e310
Lucy W <lucy.woloszczuk@gmail.com>
2024-05-23 13:09:41 -0400

    adds slide animation and spinner

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import CardGallery from "@/components/CardGallery";
 import { useEffect, useState } from "react";
 import { Card as CardType } from "@/utils/types";
 import { fetchAllCards } from "@/app/lib/data";
+import CharacterCard from "@/components/CharacterCard";
+import { Center, SimpleGrid, SlideFade, Spinner } from "@chakra-ui/react";
 
 export default function Gallery() {
   const [cards, setCards] = useState<CardType[]>([]);
@@ -17,5 +18,31 @@ export default function Gallery() {
         console.error(error);
       });
   }, []);
-  return <CardGallery cards={cards} />;
+
+  if (cards.length === 0)
+    return (
+      <Center height="100%">
+        <Spinner size="xl" thickness="3px" />
+      </Center>
+    );
+
+  return (
+    <SlideFade in={cards.length > 0}>
+      <SimpleGrid minChildWidth={300} spacing={2} width="100%">
+        {cards.map((card, i) => (
+          <CharacterCard
+            character={card}
+            key={i}
+            sx={{
+              transform: "scale(0.96)",
+              transition: "all 0.2s ease-in-out",
+              _hover: {
+                transform: "scale(1)",
+              },
+            }}
+          />
+        ))}
+      </SimpleGrid>
+    </SlideFade>
+  );
 }
