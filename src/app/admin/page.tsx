5265745edc873b5dd32904ac131675a91b28efe9
Lucy Woloszczuk <lucy.woloszczuk@gmail.com>
2024-05-31 14:40:20 -0400

    change button colour

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -118,6 +118,7 @@ export default function Admin() {
                   <Td isNumeric>
                     <Button
                       size="xs"
+                      colorScheme="blue"
                       onClick={() => {
                         updateUser(user)
                           .then(() => {
